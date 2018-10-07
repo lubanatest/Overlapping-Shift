aad39367444c62d4fc31fa74dcbbd7110260f6c6
lubanatest <32592478+lubanatest@users.noreply.github.com>
2018-10-07 20:43:37 +1100

    Update overlapping.js

--- overlapping.js
+++ overlapping.js
@@ -4,8 +4,14 @@
 /********************************************/
 
 function isOverlapping(shift1, shift2) {
+  // Two different employees
   if (shift1.employee !== shift2.employee)
     return false;
+  
+  // Assuming that:
+  // shift1.startTime < shift1.endTime AND
+  // shift2.startTime < shift2.endTime
+  // As the times are validated before executing this function
   
   return !(shift1.endTime <= shift2.startTime || shift1.startTime >= shift2.endTime);
 }
