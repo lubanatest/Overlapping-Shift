8804a3c4fbb4d1177fb740c0ab5ab48d15de7320
lubanatest <32592478+lubanatest@users.noreply.github.com>
2018-10-07 11:49:25 +1100

    Create shift.js

---
+++ shift.js
@@ -0,0 +1,19 @@
+/*****************************************/
+/*    This File includes Shift class     */
+/*****************************************/
+
+/*
+ * class Shift
+ */
+class Shift {
+  /*
+   * Constructor
+   */
+	constructor(id, employee, department, startTime, endTime){
+		this.id = id;
+		this.employee = employee;
+		this.department = department;
+		this.startTime = startTime;
+		this.endTime = endTime;
+	}
+}
